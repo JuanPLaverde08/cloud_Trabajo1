ed2078e9f85d340e7929de63488df0d431b3e547
juanlahe <juan_laverde82162@elpoli.edu.co>
2021-11-22 18:56:43 -0500

    Primer Commit del proyecto

---
+++ src/main/java/co/com/poli/cloud_trabajo1/CloudTrabajo1Application.java
@@ -0,0 +1,13 @@
+package co.com.poli.cloud_trabajo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTrabajo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTrabajo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/poli/cloud_trabajo1/CloudTrabajo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.poli.cloud_trabajo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTrabajo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
