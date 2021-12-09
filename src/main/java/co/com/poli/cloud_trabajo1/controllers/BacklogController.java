b2f603b048abc7ce8ad05c5069bbf010b15f11ed
juanlahe <juan_laverde82162@elpoli.edu.co>
2021-12-08 22:28:52 -0500

    Add 400 error for BacklogController

--- src/main/java/co/com/poli/cloud_trabajo1/controllers/BacklogController.java
+++ src/main/java/co/com/poli/cloud_trabajo1/controllers/BacklogController.java
@@ -1,10 +1,14 @@
 package co.com.poli.cloud_trabajo1.controllers;
 
 import co.com.poli.cloud_trabajo1.service.BacklogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import co.com.poli.cloud_trabajo1.entities.Backlog;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
 
 @RestController
@@ -20,7 +24,11 @@ public class BacklogController {
     }
 
     @PostMapping
-    public Backlog create(@RequestBody Backlog backlog){
+    public Backlog create(@RequestBody Backlog backlog, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Wrong parameter provided");
+        }
+
         return service.create(backlog);
     }
 }
